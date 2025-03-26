71b7c0fcbce22fb16b30058ed02cc1f3b9e4d2f9
Balaji-3009 <mbalaji3009@gmail.com>
2025-03-26 11:09:15 +0530

    minor fixes

--- api/auth.py
+++ api/auth.py
@@ -4,6 +4,11 @@ from database.session import db_dependency
 from database.models import Entry
 from fastapi import HTTPException
 
+from datetime import datetime
+
+current_datetime = datetime.now()
+current_year = current_datetime.year
+
 
 cred = credentials.Certificate("p2pmart-11931-firebase-adminsdk-fbsvc-71918906cb.json")
 firebase_admin.initialize_app(cred)
@@ -14,6 +19,11 @@ def verify_firebase_token(id_token: str):
         email = decoded_token.get("email")
         if not email.endswith("@vitstudent.ac.in"):
             raise HTTPException(status_code=403, detail="Use VIT email to login")
+        loc = email.index('@')
+        current_datetime = datetime.now()
+        current_year = current_datetime.year
+        if int(email[loc-4:loc])<current_year - 5:
+            raise HTTPException(status_code=403, detail="Only current batch can login")
         return decoded_token
     except Exception as e:
         raise ValueError(f"Invalid Firebase token: {e}")
